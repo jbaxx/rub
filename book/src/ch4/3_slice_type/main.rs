f6b2343aa14b6313eda7b071269a2d5a7070137d
jbaxx <jbaxx@users.noreply.github.com>
2022-06-07 22:43:23 -0500

    complete slice type

--- book/src/ch4/3_slice_type/main.rs
+++ book/src/ch4/3_slice_type/main.rs
@@ -42,4 +42,89 @@ fn main() {
     //
     // We're calculating values from data in a particular state that aren't tied to that state at
     // all.
+
+    // String Slices
+    // - A string slice is a reference to a part of a String
+
+    let s = String::from("hello world");
+    let _hello = &s[0..5];
+    let _world = &s[6..11];
+
+    // Rather than a reference to the entire String, hello is a reference to a portion.
+    // We create slices using a range within brackets: [starting_index..ending_index]
+    // Where ending_index is one more than the last position in the slice
+
+    // Range syntax
+    // To start at index zero you can drop the value before the two periods.
+    let _slice = &s[0..2];
+    let _slice = &s[..2];
+    // As well you can drop the trailing number
+    let len = s.len();
+    let _slice = &s[3..len];
+    let _slice = &s[3..];
+    // You can drop both values to take a slice of the entire String.
+    // These are equal:
+    let _slice = &s[0..len];
+    let _slice = &s[..];
+    // Note: String slice range indices must occure at valid UTF-8 character boundaries.
+    // Otherwise the program may exit with an error.
+
+    let mut c = String::from("hello world");
+    let _word = first_word_word(&c);
+    // c.clear(); // compile time error: as its a mutable borrow when an immutable reference exist.
+                  // and the println! uses the immutable reference later.
+    // println!("the first word is: {}", word);
+
+    // String Literals Are Slices
+    let s = "Hellow, rodl!";
+    // The type of s here is &str, it's a slice pointing to that specific point of the binary.
+    // This is also why string literals are immutable, &str is an immutable reference.
+
+    // String Slices as Parameters
+    // Knowing that you can take slices of literals and String values.
+    // The signature:
+    //     fn first_word(s: &String) -> &str
+    // Can be written as:
+    //     fn first_word(s: &str) -> &str
+    // As it allows to use the same function on both &String and &str.
+    // - If we have a string slice, we can pass it directly
+    // - If we have a String, we can pass a slice of the String or a reference tot he String.
+    // This flexibility takes advantage of deref coercions.
+
+    let my_string = String::from("hello world");
+
+    // function works on slices of String, whether partial or whole
+    let _word = first_word_word(&my_string[0..6]);
+    let _word = first_word_word(&my_string[..]);
+    // works on references so String, which are equivalent to whole slices of String
+    let _word = first_word_word(&my_string);
+
+    let my_string_literal = "hello world";
+    // function works on slices of string literal, wheter partial or whole
+    let _word = first_word_word(&my_string_literal[0..6]);
+    let _word = first_word_word(&my_string_literal[..]);
+    // because string literals *are* string slices already,
+    // this works too, wihtout the slice syntax
+    let _word = first_word_word(&my_string_literal);
+
+    // Other Slices
+    // String slices are specific to strings, but there's a more general slice type.
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2, 3]);
+    // This slice has the type &[i32].
+}
+
+// Rewriting the function
+fn first_word_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    // &s[..]
+    s
 }
