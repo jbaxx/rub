714cc7e81fc8d38edaa777a5f33deba2adfd49c3
jbaxx <jbaxx@users.noreply.github.com>
2022-06-20 22:38:43 -0500

    update

---
+++ book/src/ch7/restaurant/src/lib.rs
@@ -0,0 +1,24 @@
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
